b13bf78b4b0b2c06bfcd461428d7db4658308f1f
erfan <landa.cloner1379@gmail.com>
2026-08-04 19:07:54 +0330

    add constraints

--- modules/wallet/models.py
+++ modules/wallet/models.py
@@ -2,11 +2,25 @@ from modules.wallet.enums import TransactionCategory , TransactionType
 from django.db import models
 
 class Wallet(models.Model):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(balance__gte=0),
+                name="wallet_balance_amount_gte_zero"
+            )
+        ]
+
     balance = models.PositiveBigIntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class TransactionIdempotencyKey(models.Model):
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["wallet"] , name="wallet_index"),
+        ]
+
     wallet = models.ForeignKey(Wallet, on_delete=models.PROTECT, related_name="wallet")
     idempotency_key = models.CharField(max_length=100 , unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -32,5 +46,3 @@ class Transaction(models.Model):
     balance_before = models.BigIntegerField(default=0)
     balance_after = models.BigIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    models.CheckConstraint(condition=models.Q(amount__gt=0) , name="transaction_amount_gt_zero")
